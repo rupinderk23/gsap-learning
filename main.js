ab98a19189f01601610b28371e836b667197b269
rupinderk23 <rupinder@njgraphica.com>
2024-01-04 17:10:22 +0530

    locomotive scroll added

--- main.js
+++ main.js
@@ -1,5 +1,34 @@
 gsap.registerPlugin(ScrollTrigger);
 
+// --- SETUP START ---
+// Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+const locoScroll = new LocomotiveScroll({
+el: document.querySelector(".smooth-scroll"),
+    smooth: true,
+    multiplier: 1.5
+});
+// each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+locoScroll.on("scroll", ScrollTrigger.update);
+
+// tell ScrollTrigger to use these proxy methods for the ".smooth-scroll" element since Locomotive Scroll is hijacking things
+ScrollTrigger.scrollerProxy(".smooth-scroll", {
+scrollTop(value) {
+    return arguments.length ? locoScroll.scrollTo(value, {duration: 0, disableLerp: true}) : locoScroll.scroll.instance.scroll.y;
+}, // we don't have to define a scrollLeft because we're only scrolling vertically.
+getBoundingClientRect() {
+    return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+},
+// LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+pinType: document.querySelector(".smooth-scroll").style.transform ? "transform" : "fixed"
+});
+
+// each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
+ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+ScrollTrigger.defaults({ scroller: ".smooth-scroll" });
+// --- SETUP END ---
+  
+
+
 ScrollTrigger.matchMedia({
     // desktop
     "(min-width: 1367px)": function () {
@@ -49,7 +78,11 @@ ScrollTrigger.matchMedia({
     }
 })
 
-// scroll magic
+// smooth scroll
+
+  
+
+// scroll magic - move to next section on mousewheel
 
 // var ctrl = new ScrollMagic.Controller({   
 // });
